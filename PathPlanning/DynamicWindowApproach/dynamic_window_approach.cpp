85711364794e49be9691ce87469450f9e0b1ec1f
RenFukatsu <fukatsuren88@gmail.com>
2019-04-27 16:00:02 +0900

    dwa

--- PathPlanning/DynamicWindowApproach/dynamic_window_approach.cpp
+++ PathPlanning/DynamicWindowApproach/dynamic_window_approach.cpp
@@ -59,41 +59,19 @@ struct Coordinates
     double y; // [m]
 };
 
-std::vector<double> calc_dynamic_window(RobotState x, Config config)
+struct Speeds
 {
-    // Dynamic window from robot specification
-    std::vector<double> Vs = {
-        config.min_speed, config.max_speed,
-        -config.max_yawrate, config.max_yawrate
-    };
+    double v; // [m/s]
+    double omega; // [rad]
+};
 
-    // Dynamic window from motion model
-    std::vector<double> Vd = {
-        x.v - config.max_accel * config.dt,
-        x.v + config.max_accel * config.dt,
-        x.omega - config.max_dyawrate * config.dt,
-        x.omega + config.max_dyawrate * config.dt 
-    };
-
-    // {vmin, vmax, yawrate min, yawrate max}
-    std::vector<double> dw = {
-        std::max(Vs[0], Vd[0]), std::min(Vs[1], Vd[1]),
-        std::max(Vs[2], Vd[2]), std::min(Vs[3], Vd[3])
-    };
-    return dw;
-}
-
-// Dynamic Window control
-std::tuple<Coordinates, RobotState> dwa_control(RobotState x, Coordinates u, Config config, Coordinates goal, std::vector<Coordinates> ob)
-{
-    RobotState ltraj = {0.0, 0.0, 0.0, 0.0, 0.0};
-
-    std::vector<double> dw = calc_dynamic_window(x, config);
-
-    std::tie(u, traj = )
-
-    return std::forward_as_tuple(u, traj);
-}
+RobotState motion(RobotState x, Speeds u, double dt);
+std::vector<double> calc_dynamic_window(RobotState x, Config config);
+std::vector<RobotState> calc_trajectory(RobotState xinit, double v, double y, Config config);
+std::tuple<Speeds, std::vector<RobotState>> calc_final_input(RobotState x, Speeds u, std::vector<double> dw, Config config, Coordinates goal, std::vector<Coordinates> ob);
+double calc_obstacle_cost(std::vector<RobotState> traj, std::vector<Coordinates> ob, Config config);
+double calc_to_goal_cost(std::vector<RobotState> traj, Coordinates goal, Config config);
+std::tuple<Speeds, std::vector<RobotState>> dwa_control(RobotState x, Speeds u, Config config, Coordinates goal, std::vector<Coordinates> ob);
 
 int main()
 {
@@ -119,16 +97,160 @@ int main()
         {12.0, 12.0}
     };
 
-    Coordinates u = {0.0, 0.0};
+    Speeds u = {0.0, 0.0};
     Config config;
-    RobotState traj = x;
+    std::vector<RobotState> traj = {x};
 
-    for(int i=0; i<10; i++){ // あとで1000に直す
-        RobotState ltraj;
+    for(int i=0; i<1; i++){ // あとで1000に直す
+        std::vector<RobotState> ltraj;
         std::tie(u, ltraj) = dwa_control(x, u, config, goal, ob);
 
+        std::cout << "aa" << std::endl;
     }
 
     
     return 0;
+}
+
+
+RobotState motion(RobotState x, Speeds u, double dt)
+{   // motion model 
+
+    x.yaw += u.omega * dt;
+    x.x += u.v * cos(x.yaw) * dt;
+    x.y += u.v * sin(x.yaw) * dt;
+    x.v = u.v;
+    x.omega = u.omega;
+
+    return x;
+}
+
+std::vector<double> calc_dynamic_window(RobotState x, Config config)
+{
+    // Dynamic window from robot specification
+    std::vector<double> Vs = {
+        config.min_speed, config.max_speed,
+        -config.max_yawrate, config.max_yawrate
+    };
+
+    // Dynamic window from motion model
+    std::vector<double> Vd = {
+        x.v - config.max_accel * config.dt,
+        x.v + config.max_accel * config.dt,
+        x.omega - config.max_dyawrate * config.dt,
+        x.omega + config.max_dyawrate * config.dt 
+    };
+
+    // {vmin, vmax, yawrate min, yawrate max}
+    std::vector<double> dw = {
+        std::max(Vs[0], Vd[0]), std::min(Vs[1], Vd[1]),
+        std::max(Vs[2], Vd[2]), std::min(Vs[3], Vd[3])
+    };
+    return dw;
+}
+
+std::vector<RobotState> calc_trajectory(RobotState xinit, double v, double y, Config config)
+{
+    RobotState x = xinit;
+    std::vector<RobotState> traj = {x};
+    double time = 0.0;
+
+    while(time <= config.predict_time){
+        std::cout<<1<<std::endl;
+
+        Speeds u = {v, y};
+        x = motion(x, u, config.dt);
+        traj.emplace_back(x);
+        time += config.dt;
+    }
+
+    return traj;
+}
+
+std::tuple<Speeds, std::vector<RobotState>> calc_final_input(RobotState x, Speeds u, std::vector<double> dw,
+                                                     Config config, Coordinates goal, std::vector<Coordinates> ob)
+{
+    RobotState xinit = x;
+    double min_cost = 10000.0;
+    Speeds min_u = u;
+    min_u.v = 0.0;
+    std::vector<RobotState> best_traj = {x};
+
+    // evalucate all trajectory with sampled input in dynamic window
+    for(double v = dw[0]; v < dw[1]; v += config.v_reso){
+        for(double y = dw[2]; y < dw[3]; config.yawrate_reso){
+            std::cout<<2<<std::endl;
+            std::vector<RobotState> traj =calc_trajectory(xinit, v, y, config);
+
+            // calc cost
+            double to_goal_cost = calc_to_goal_cost(traj, goal, config);
+            double speed_cost = config.speed_cost_gain * (config.max_speed - traj.back().v);
+            double ob_cost = calc_obstacle_cost(traj, ob, config);
+
+            double final_cost = to_goal_cost + speed_cost + ob_cost;
+
+            // search minimum trajectory
+            if(min_cost >= final_cost){
+                min_cost = final_cost;
+                min_u.v = v;
+                min_u.omega = y;
+                best_traj = traj;
+            }
+        }
+    } 
+
+    return std::forward_as_tuple(min_u, best_traj);
+}
+
+double calc_obstacle_cost(std::vector<RobotState> traj, std::vector<Coordinates> ob, Config config)
+{   // calc obstacle cost inf: collistion, 0:free
+    
+    const int skip_n = 2;
+    double minr = INFINITY;
+
+    for(int ii=0; ii<traj.size(); ii += skip_n){
+        for(int i=0; i<ob.size(); i++){
+            std::cout<<3<<std::endl;
+            double ox = ob[i].x;
+            double oy = ob[i].y;
+            double dx = traj[ii].x - ox;
+            double dy = traj[ii].y - oy;
+
+            double r = sqrt(pow(dx, 2.0) + pow(dy, 2.0));
+            if( r <= config.robot_radius){
+                return INFINITY;
+            }
+            if( minr >= r){
+                minr = r;
+            }
+        }
+    }
+
+    return 1.0 / minr;
+}
+
+double calc_to_goal_cost(std::vector<RobotState> traj, Coordinates goal, Config config)
+{   // calc to goal cost. It is 2D norm.
+
+    double goal_magnitude = sqrt(pow(goal.x, 2.0) + pow(goal.y, 2.0) );
+    double traj_magnitude = sqrt(pow(traj.back().x, 2.0) + pow(traj.back().y, 2.0) );
+    double dot_product = (goal.x * traj.back().x) + (goal.y * traj.back().y);
+    double error = dot_product / (goal_magnitude * traj_magnitude);
+    double error_angle = acos(error);
+    double cost = config.to_goal_cost_gain * error_angle;
+
+    return cost;
+}
+
+// Dynamic Window control
+std::tuple<Speeds, std::vector<RobotState>> dwa_control(RobotState x, Speeds u, Config config, Coordinates goal, std::vector<Coordinates> ob)
+{
+    RobotState ltraj = {0.0, 0.0, 0.0, 0.0, 0.0};
+
+    std::vector<double> dw = calc_dynamic_window(x, config);
+
+    std::vector<RobotState> traj = {};
+    std::tie(u, traj) = calc_final_input(x, u, dw, config, goal, ob);
+
+    return std::forward_as_tuple(u, traj);
 }
